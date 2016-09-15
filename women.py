5b7abcdfdb155af19b988d834ad20f8d3c865898
Hugo <nsirone@outlook.com>
2016-09-15 22:42:05 +0800

    ac

--- women.py
+++ women.py
@@ -1,1 +1,16 @@
-sdff
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#   Author  :   Hugo
+#   E-mail  :   nsirone@outlook.com
+#   Date    :   16/09/15 22:37:58
+
+
+import os
+import urllib2
+
+url = 'http://www.baidu.com'
+request = urllib2.Request(url)
+response = urllib2.urlopen(request)
+
+print response.read()
